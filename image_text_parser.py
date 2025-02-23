367dec5799a08a9fd60ffc05b897a5a7c4b98ece
Tyler Wong <55007080+ty730@users.noreply.github.com>
2025-02-22 21:47:19 -0600

    clean up more

--- image_text_parser.py
+++ image_text_parser.py
@@ -87,7 +87,7 @@ def getActivityDetails(text, activity, activity_name, is_existing_task):
     num_tasks = 0
     task_list = text.split('\n')
     for line in task_list:
-        if not line.strip() or any(ext in task_name for ext in ['©', 'tt a @']):
+        if not line.strip() or any(ext in line for ext in ['©', 'tt a @']):
             is_prev_task_name = False
             continue
         task_name_index = line.find(')')
