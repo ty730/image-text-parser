8d4331eb0105b83edf7a16bd657589e448264ed4
Tyler Wong <55007080+ty730@users.noreply.github.com>
2025-02-22 21:49:31 -0600

    Add extra string check

--- image_text_parser.py
+++ image_text_parser.py
@@ -87,7 +87,7 @@ def getActivityDetails(text, activity, activity_name, is_existing_task):
     num_tasks = 0
     task_list = text.split('\n')
     for line in task_list:
-        if not line.strip() or any(ext in line for ext in ['©', 'tt a @']):
+        if not line.strip() or any(ext in line for ext in ['©', 'tt a @']) or line.strip() == 'oO' or line.strip() == 'Hy':
             is_prev_task_name = False
             continue
         task_name_index = line.find(')')
